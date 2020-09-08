6d2610bc2f6c26557c97e9fb289b1a723978506c
DaveRoppo <roppo.david@gmail.com>
2020-09-08 15:05:13 -0500

    Fix bash

--- guessinggame.sh
+++ guessinggame.sh
@@ -6,9 +6,9 @@
 #create while loop that checks if user guess matches files variable
 #create if/elif/else statement to tell user if guess is too high/low/spot on
 #break loop when user guesses correctly
-#Not much repeated code here.. but lets create a function to echo "guess again"
+#Not much repeated code here.. but lets create a function to echo "guess again" 
 
-files=$(ls | wc -l)
+files=$(ls | wc -l) 
 
 function guess {
   echo "Guess again."
@@ -18,16 +18,25 @@ while [[ $answer != $files ]]
 do
   echo "How many files are in the current directory?"
   read answer
-  if [[ $answer -gt $files ]]
+  if [[ $answer =~ [0-9]+ ]]
   then
-    echo "Too high."
-    guess
-  elif [[ $answer -lt $files ]]
-  then
-    echo "Too low."
-    guess
+    if [[ $answer -gt $files ]]
+    then
+      echo "Too high."
+      guess
+    elif [[ $answer -lt $files ]]
+    then
+      echo "Too low."
+      guess
+    elif [[ $answer -eq $files ]]
+    then
+      echo "Got it!"
+    fi
   else
-    echo "Got it!"
+    echo "Please enter your guess by using the number pad"
   fi
 done
+
+
+
 
